eb52f4cf88d7369a8503b200ea7955b3727f0f23
Keesun Baik <whiteship2000@gmail.com>
2019-07-24 11:53:01 -0700

    Add Spring Security Config

---
+++ src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package me.whiteship.demospringsecurityform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/info").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
+    }
+}
