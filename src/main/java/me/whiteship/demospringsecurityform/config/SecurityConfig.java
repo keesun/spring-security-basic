858354ef00218d482fd2185230e3304f7a09f1c0
Keesun Baik <whiteship2000@gmail.com>
2019-07-24 14:03:16 -0700

    Add InMemory Users

--- src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.whiteship.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,5 +18,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http.formLogin();
         http.httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("keesun").password("{noop}123").roles("USER").and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
     }
 }
