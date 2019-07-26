a9854bc80c2e6b3026b4b00432e290dafef3b45f
Keesun Baik <whiteship2000@gmail.com>
2019-07-26 15:56:26 -0700

    SecurityContextHolder and Authentication

--- src/main/java/me/whiteship/demospringsecurityform/form/SampleController.java
+++ src/main/java/me/whiteship/demospringsecurityform/form/SampleController.java
@@ -1,5 +1,6 @@
 package me.whiteship.demospringsecurityform.form;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,8 @@ import java.security.Principal;
 
 @Controller
 public class SampleController {
+
+    @Autowired SampleService sampleService;
 
     @GetMapping("/")
     public String index(Model model, Principal principal) {
@@ -29,6 +32,7 @@ public class SampleController {
     @GetMapping("/dashboard")
     public String dashboard(Model model, Principal principal) {
         model.addAttribute("message", "Hello " + principal.getName());
+        sampleService.dashboard();
         return "dashboard";
     }
 
---
+++ src/main/java/me/whiteship/demospringsecurityform/form/SampleService.java
@@ -0,0 +1,20 @@
+package me.whiteship.demospringsecurityform.form;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class SampleService {
+
+    public void dashboard() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Object credentials = authentication.getCredentials();
+        boolean authenticated = authentication.isAuthenticated();
+    }
+}
