736beda713b58138bc56c76ab3851ac04d7adfac
Keesun Baik <whiteship2000@gmail.com>
2019-08-04 22:08:05 -0700

    LogoutFilter

--- src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -51,6 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.httpBasic();
 
+        http.logout().logoutSuccessUrl("/");
+
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
